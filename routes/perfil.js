8277098442f423bfdaf2592a3e0b8a2a128278f0
rf190598-eng <rf190598@gmail.com>
2026-07-02 22:32:53 -0300

    Replace verificarTemp with selfie-verificacao route

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { editarPerfil, uploadFoto, upload, atualizarLocalizacao, verificarTemp } = require('../controllers/perfilController');
+const { editarPerfil, uploadFoto, upload, atualizarLocalizacao, uploadSelfieVerificacao } = require('../controllers/perfilController');
 const { autenticar } = require('../controllers/authMiddleware');
 router.put('/editar', autenticar, editarPerfil);
 router.post('/foto', autenticar, upload.single('foto'), uploadFoto);
 router.put('/localizacao', autenticar, atualizarLocalizacao);
-router.put('/verificar-temp', autenticar, verificarTemp);
+router.post('/selfie-verificacao', autenticar, upload.single('foto'), uploadSelfieVerificacao);
 module.exports = router;
