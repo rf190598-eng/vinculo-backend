75e99a5268a1f93350ef56d5c16377d545d8eb77
rf190598-eng <rf190598@gmail.com>
2026-07-03 22:48:31 -0300

    Add routes for security-related operations

---
+++ routes/seguranca.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {
+  listarContatos, criarContato, removerContato,
+  dispararPanico,
+  iniciarSessao, atualizarLocalizacaoSessao, confirmarRetornoSeguro, statusSessao
+} = require('../controllers/segurancaController');
+const { autenticar } = require('../controllers/authMiddleware');
+
+router.get('/contatos', autenticar, listarContatos);
+router.post('/contatos', autenticar, criarContato);
+router.delete('/contatos/:id', autenticar, removerContato);
+
+router.post('/panico', autenticar, dispararPanico);
+
+router.post('/sessao/iniciar', autenticar, iniciarSessao);
+router.put('/sessao/localizacao', autenticar, atualizarLocalizacaoSessao);
+router.post('/sessao/confirmar', autenticar, confirmarRetornoSeguro);
+router.get('/sessao/status', autenticar, statusSessao);
+
+module.exports = router;
