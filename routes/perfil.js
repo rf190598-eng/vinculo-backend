45519c3cbf85f9fb75834e8892411766b9429cc8
rf190598-eng <rf190598@gmail.com>
2026-07-04 23:17:11 -0300

    Add route for indication statistics
    Added route for fetching indication statistics.

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { editarPerfil, uploadFoto, upload, atualizarLocalizacao, uploadSelfieVerificacao } = require('../controllers/perfilController');
+const { editarPerfil, uploadFoto, upload, atualizarLocalizacao, uploadSelfieVerificacao, estatisticasIndicacao } = require('../controllers/perfilController');
 const { autenticar } = require('../controllers/authMiddleware');
 router.put('/editar', autenticar, editarPerfil);
 router.post('/foto', autenticar, upload.single('foto'), uploadFoto);
 router.put('/localizacao', autenticar, atualizarLocalizacao);
 router.post('/selfie-verificacao', autenticar, upload.single('foto'), uploadSelfieVerificacao);
+router.get('/indicacoes', autenticar, estatisticasIndicacao);
 module.exports = router;
