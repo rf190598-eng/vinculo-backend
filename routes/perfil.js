e976a514b23e66c0301b45f4309c09567d58302d
rf190598-eng <rf190598@gmail.com>
2026-07-17 17:49:06 -0300

    Add gallery photo management routes

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { editarPerfil, uploadFoto, upload, atualizarLocalizacao, uploadSelfieVerificacao, estatisticasIndicacao } = require('../controllers/perfilController');
+const {
+  editarPerfil, uploadFoto, upload, atualizarLocalizacao, uploadSelfieVerificacao, estatisticasIndicacao,
+  listarFotosGaleria, adicionarFotoGaleria, removerFotoGaleria
+} = require('../controllers/perfilController');
 const { autenticar } = require('../controllers/authMiddleware');
+
 router.put('/editar', autenticar, editarPerfil);
 router.post('/foto', autenticar, upload.single('foto'), uploadFoto);
 router.put('/localizacao', autenticar, atualizarLocalizacao);
 router.post('/selfie-verificacao', autenticar, upload.single('foto'), uploadSelfieVerificacao);
 router.get('/indicacoes', autenticar, estatisticasIndicacao);
+
+router.get('/galeria', autenticar, listarFotosGaleria);
+router.post('/galeria', autenticar, upload.single('foto'), adicionarFotoGaleria);
+router.delete('/galeria/:id', autenticar, removerFotoGaleria);
+
 module.exports = router;
