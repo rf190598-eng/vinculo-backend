311ec5244eb62013805d16fdaa6a1bbbc62bcba4
rf190598-eng <rf190598@gmail.com>
2026-07-27 15:39:00 -0300

    Add prompts field to editarPerfil function

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -34,7 +34,7 @@ const editarPerfil = async (req, res) => {
   try {
     const {
       nome, bio, genero, data_nascimento, cidade, objetivo, signo,
-      altura, peso, cor_cabelo, estilo_vida, interesses,
+      altura, peso, cor_cabelo, estilo_vida, interesses, prompts,
       pref_genero, pref_idade_min, pref_idade_max, pref_distancia_km,
       pref_altura_min, pref_altura_max, pref_peso_min, pref_peso_max, pref_cor_cabelo
     } = req.body;
@@ -49,6 +49,7 @@ const editarPerfil = async (req, res) => {
     if (signo) dados.signo = signo;
     if (estilo_vida !== undefined) dados.estilo_vida = estilo_vida;
     if (interesses !== undefined) dados.interesses = interesses;
+    if (prompts !== undefined) dados.prompts = prompts;
     if (altura !== undefined) dados.altura = altura || null;
     if (peso !== undefined) dados.peso = peso || null;
     if (cor_cabelo !== undefined) dados.cor_cabelo = cor_cabelo || null;
