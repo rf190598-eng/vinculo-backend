0d72563d65ceda800084f2699d1cac6104f29ec6
rf190598-eng <rf190598@gmail.com>
2026-07-30 21:16:49 -0300

    Add rate limiting to registration and login routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { cadastrar, login, perfil } = require('../controllers/authController');
 const { autenticar } = require('../controllers/authMiddleware');
+const { limitarTaxa } = require('../utils/limitadorTaxa');
 
 // Rotas públicas
-router.post('/cadastrar', cadastrar);
-router.post('/login', login);
+router.post('/cadastrar', limitarTaxa({ maxTentativas: 8 }), cadastrar);
+router.post('/login', limitarTaxa({ maxTentativas: 10 }), login);
 
 // Rotas protegidas
 router.get('/perfil', autenticar, perfil);
 
-module.exports = router; 
+module.exports = router;
