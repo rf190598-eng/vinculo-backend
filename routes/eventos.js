5f45c44a6c6fba901e74f2bc33271377635c6c16
rf190598-eng <rf190598@gmail.com>
2026-07-31 21:34:21 -0300

    Update eventos.js

--- routes/eventos.js
+++ routes/eventos.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { listarEventos, criarEvento, confirmarPresenca } = require('../controllers/eventoController');
 const { autenticar } = require('../controllers/authMiddleware');
+const { verificarAdmin } = require('../controllers/verificarAdmin');
 
 router.get('/', autenticar, listarEventos);
-router.post('/', autenticar, criarEvento);
+router.post('/', autenticar, verificarAdmin, criarEvento);
 router.post('/:evento_id/confirmar', autenticar, confirmarPresenca);
 
 module.exports = router;
