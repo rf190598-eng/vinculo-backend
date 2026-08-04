35f77e1784348db56576e58ac415a45abaf27a89
rf190598-eng <rf190598@gmail.com>
2026-08-04 10:20:15 -0300

    Remove selfie verification route

--- routes/perfil.js
+++ routes/perfil.js
@@ -9,7 +9,7 @@ const { autenticar } = require('../controllers/authMiddleware');
 router.put('/editar', autenticar, editarPerfil);
 router.post('/foto', autenticar, upload.single('foto'), uploadFoto);
 router.put('/localizacao', autenticar, atualizarLocalizacao);
-router.post('/selfie-verificacao', autenticar, upload.single('foto'), uploadSelfieVerificacao);
+
 router.get('/indicacoes', autenticar, estatisticasIndicacao);
 
 router.get('/galeria', autenticar, listarFotosGaleria);
